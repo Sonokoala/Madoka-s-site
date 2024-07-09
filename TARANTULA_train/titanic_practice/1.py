bafd80902d73e05e5b981836b0f706c000887854
Ryota_s <cangtaizhang@gmail.com>
2024-07-09 21:35:21 +0900

    Resolved merge conflicts

---
+++ TARANTULA_train/titanic_practice/1.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt
+
+train = pd.read_csv("C:\Users\81904\TARANTULA_train\titanic_data\train.csv")
+test = pd.read_csv("C:\Users\81904\TARANTULA_train\titanic_data\test.csv")
