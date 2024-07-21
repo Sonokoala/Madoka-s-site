a1ebcd65acf3eb514bc72a9caf0bbb6788bb91d6
Io-re0123 <149930468+Io-re0123@users.noreply.github.com>
2024-07-21 16:01:18 +0900

    Delete TARANTULA_train directory
    修正

--- TARANTULA_train/titanic_practice/1.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import pandas as pd 
-import matplotlib.pyplot as plt
-
-train = pd.read_csv("C:\Users\81904\TARANTULA_train\titanic_data\train.csv")
-test = pd.read_csv("C:\Users\81904\TARANTULA_train\titanic_data\test.csv")
